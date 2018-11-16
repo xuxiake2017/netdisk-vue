9699daff8e49cc5acc8bfba7264fef8c78568183
xuxiake2016 <xuxiake2016@163.com>
2018-11-16 17:56:24 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './common/request'
+import router from './router'
+// import store from '@/store'
+
+Vue.use(ElementUI);
+
+Vue.prototype.$loading = ElementUI.Loading.service;
+Vue.prototype.$msgbox = ElementUI.MessageBox;
+Vue.prototype.$alert = ElementUI.MessageBox.alert;
+Vue.prototype.$confirm = ElementUI.MessageBox.confirm;
+Vue.prototype.$prompt = ElementUI.MessageBox.prompt;
+Vue.prototype.$notify = ElementUI.Notification;
+Vue.prototype.$message = ElementUI.Message;
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+  render: h => h(App)
+})
